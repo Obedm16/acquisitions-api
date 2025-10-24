1afddba04d130df1a5cfd0b0379735f179c0a15a
Obedm16 <oduoliobed@protonmail.com>
2025-10-24 16:25:20 +0300

    server setup

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+// const PORT = process.env.PORT || 4001;
+
+// app.listen(PORT, () => {
+//   console.log(`Listening on port ${PORT}...`)
+// })
+
+app.get("/", (req, res) => {
+  res.status(200).send("You are interating with acquisitions!");
+});
+
+export default app;
---
+++ src/server.js
@@ -0,0 +1,9 @@
+// import express from 'express';
+import app from "./app.js";
+// const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port http://localhost:${PORT}`);
+});
