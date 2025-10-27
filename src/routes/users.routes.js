7c26ff39711d325302d65ea27bd01fafce984939
Obedm16 <oduoliobed@protonmail.com>
2025-10-27 22:03:51 +0300

    Implementing CI/CD workflows

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { fetchAllUsers, getUserByIdController, updateUser, deleteUser } from '#controllers/users.controller.js';
+import {
+  fetchAllUsers,
+  getUserByIdController,
+  updateUser,
+  deleteUser,
+} from '#controllers/users.controller.js';
+import { authenticateToken, requireRole } from '#middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/', fetchAllUsers);
-router.get('/:id', getUserByIdController);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.get('/', authenticateToken, fetchAllUsers);
+router.get('/:id', authenticateToken, getUserByIdController);
+router.put('/:id', authenticateToken, updateUser);
+router.delete('/:id', authenticateToken, requireRole['admin'], deleteUser);
 
 export default router;
